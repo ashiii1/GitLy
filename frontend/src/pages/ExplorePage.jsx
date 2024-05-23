1d42f7b544d6bad2036768db834ce2b34aaef77d
ashiii1 <aashishaik526@gmail.com>
2024-05-23 18:57:40 +0530

    explore page implemented

--- frontend/src/pages/ExplorePage.jsx
+++ frontend/src/pages/ExplorePage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { useState } from "react";
+import toast from "react-hot-toast";
+import Spinner from "../components/Spinner";
+import Repos from "../components/Repos";
 
 const ExplorePage = () => {
-  return (
+	// https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=10
+	const [loading, setLoading] = useState(false);
+	const [repos, setRepos] = useState([]);
+	const [selectedLanguage, setSelectedLanguage] = useState("");
 
+	const exploreRepos = async (language) => {
+		setLoading(true);
+		setRepos([]);
+		try {
+			const res = await fetch(`https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc&per_page=10`);
+			const data = await res.json();
+			setRepos(data.items);
+
+			setSelectedLanguage(language);
+		} catch (error) {
+			toast.error(error.message);
+		} finally {
+			setLoading(false);
+		}
+	};
+	return (
 		<div className='px-4'>
 			<div className='bg-glass max-w-2xl mx-auto rounded-md p-4'>
 				<h1 className='text-xl font-bold text-center'>Explore Popular Repositories</h1>
@@ -11,35 +33,45 @@ const ExplorePage = () => {
 						src='/javascript.svg'
 						alt='JavaScript ogo'
 						className='h-11 sm:h-20 cursor-pointer'
-					//	onClick={() => exploreRepos("javascript")}
+						onClick={() => exploreRepos("javascript")}
 					/>
 					<img
 						src='/typescript.svg'
 						alt='TypeScript logo'
 						className='h-11 sm:h-20 cursor-pointer'
-					//	onClick={() => exploreRepos("typescript")}
+						onClick={() => exploreRepos("typescript")}
 					/>
 					<img
 						src='/c++.svg'
 						alt='C++ logo'
 						className='h-11 sm:h-20 cursor-pointer'
-					//	onClick={() => exploreRepos("c++")}
+						onClick={() => exploreRepos("c++")}
 					/>
 					<img
 						src='/python.svg'
 						alt='Python logo'
 						className='h-11 sm:h-20 cursor-pointer'
-					//	onClick={() => exploreRepos("python")}
+						onClick={() => exploreRepos("python")}
 					/>
 					<img
 						src='/java.svg'
 						alt='Java logo'
 						className='h-11 sm:h-20 cursor-pointer'
-					//	onClick={() => exploreRepos("java")}
+						onClick={() => exploreRepos("java")}
 					/>
 				</div>
-        </div>
-</div>          )
-}
-
-export default ExplorePage
+				{repos.length > 0 && (
+					<h2 className='text-lg font-semibold text-center my-4'>
+						<span className='bg-blue-100 text-blue-800 font-medium me-2 px-2.5 py-0.5 rounded-full '>
+							{selectedLanguage.toUpperCase()}{" "}
+						</span>
+						Repositories
+					</h2>
+				)}
+				{!loading && repos.length > 0 && <Repos repos={repos} alwaysFullWidth />}
+				{loading && <Spinner />}
+			</div>
+		</div>
+	);
+};
+export default ExplorePage;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,113 +1,3 @@
-// // import toast from "react-hot-toast"
-// // import SortRepos from "../components/SortRepos"
-// // import ProfileInfo from '../components/ProfileInfo'
-// // import Search from "../components/Search"
-// // import Repos from '../components/Repos'
-// // import { useCallback, useEffect, useState } from "react"
-
-
-// // const HomePage = () => {
-// //  const [userProfile, setUserProfile]=useState(null);
-// //  const [repos, setRepos]=useState([]);
-// //  const [loading, setLoading]=useState(false);
-
-// //  const [sortType, setSortType]=useState("forks");
-
-
-// //  const getUserProfileAndREpos=useCallback(
-// //   async()=>{
-// //     setLoading(true)
-// //     try{
-// //      const userRes=await fetch("https://api.github.com/users/burakorkmez")
-// //      const userProfile=await userRes.json()
-// //   setUserProfile(userProfile)
-  
-// //   const repoRes=await fetch(userProfile.repos_url)
-// //   const repos=await repoRes.json();
-// //   setRepos(repos)
-// //  console.log("userProfile:", userProfile)
-// //  console.log("repos:", repos)
-  
-// //     }catch(error){
-// //       toast.error(error.message)
-// //     }finally{
-// //       setLoading(false)
-// //     }
-// //    },[]
-// //  )
-
-
-// //  useEffect(()=>{
-// //   getUserProfileAndREpos()
-// //  },[getUserProfileAndREpos])
-
-
-// //   return (
-// //      <div className="m-4">
-// //       <Search/>
-// //       <SortRepos/>
-// //       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-// //         <ProfileInfo userProfile={userProfile}/>
-        
-// //       <Repos/>
-// //       </div>
-// //      </div>
-
-// //   )
-// // }
-
-// // export default HomePage
-
-// import toast from "react-hot-toast";
-// import SortRepos from "../components/SortRepos";
-// import ProfileInfo from "../components/ProfileInfo";
-// import Search from "../components/Search";
-// import Repos from "../components/Repos";
-// import { useCallback, useEffect, useState } from "react";
-
-// const HomePage = () => {
-//   const [userProfile, setUserProfile] = useState(null);
-//   const [repos, setRepos] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [sortType, setSortType] = useState("forks");
-
-//   const getUserProfileAndRepos = useCallback(async () => {
-//     setLoading(true);
-//     try {
-//       const userRes = await fetch("https://api.github.com/users/ashiii1");
-//       const userProfile = await userRes.json();
-//       setUserProfile(userProfile);
-
-//       const repoRes = await fetch(userProfile.repos_url);
-//       const repos = await repoRes.json();
-//       setRepos(repos);
-
-//       console.log("userProfile:", userProfile);
-//       console.log("repos:", repos);
-//     } catch (error) {
-//       toast.error(error.message);
-//     } finally {
-//       setLoading(false);
-//     }
-//   }, []);
-
-//   useEffect(() => {
-//     getUserProfileAndRepos();
-//   }, [getUserProfileAndRepos]);
-
-//   return (
-//     <div className="m-4">
-//       <Search />
-//       <SortRepos />
-//       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-//         <ProfileInfo userProfile={userProfile} />
-//         <Repos repos={repos} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default HomePage;
 import toast from "react-hot-toast";
 import SortRepos from "../components/SortRepos";
 import Search from "../components/Search";
@@ -125,7 +15,11 @@ const HomePage = () => {
   const getUserProfileAndRepos = useCallback(async (username="ashiii1") => {
     setLoading(true);
     try {
-      const userRes = await fetch(`https://api.github.com/users/${username}`);
+      const userRes = await fetch(`https://api.github.com/users/${username}`, {
+    headers: {
+      Authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
+    },
+      });
       const userProfile = await userRes.json();
       setUserProfile(userProfile);
 
@@ -159,11 +53,11 @@ const HomePage = () => {
   }
   const onSort = (sortType) => {
 		if (sortType === "recent") {
-			repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, recent first
+			repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); 
 		} else if (sortType === "stars") {
-			repos.sort((a, b) => b.stargazers_count - a.stargazers_count); //descending, most stars first
+			repos.sort((a, b) => b.stargazers_count - a.stargazers_count); 
 		} else if (sortType === "forks") {
-			repos.sort((a, b) => b.forks_count - a.forks_count); //descending, most forks first
+			repos.sort((a, b) => b.forks_count - a.forks_count); 
 		}
 		setSortType(sortType);
 		setRepos([...repos]);
