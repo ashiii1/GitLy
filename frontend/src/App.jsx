8e708b78673892ee99aeccbcd45852b455706d34
Sk Ashi <aashishaik526@gmail.com>
2024-08-24 19:08:35 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,33 +5,24 @@ import HomePage from './pages/HomePage';
 import ExplorePage from './pages/ExplorePage';
 import LikesPage from './pages/LikesPage';
 import Sidebar from './components/Sidebar';
+
 function App() {
   return (
-      <div className="flex">
-       
-          <Routes>
-            <Route
-              path='/'
-              element={
-                  <HomePage />
-              }
-            />
-            <Route
-              path='/explore'
-              element={
-                  <ExplorePage />
-              }
-            />
-            <Route
-              path='/likes'
-              element={
-                  <LikesPage />
-              }
-            />
-          </Routes>
-          <Toaster />
-        </div>
-    
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <Sidebar className="w-64 bg-gray-800 text-white h-full fixed" />
+
+      {/* Main content */}
+      <div className="flex-1 ml-12 p-4 overflow-auto">
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/explore' element={<ExplorePage />} />
+          <Route path='/likes' element={<LikesPage />} />
+        </Routes>
+        <Toaster />
+      </div>
+    </div>
   );
 }
+
 export default App;
